a5d787300d5fb342af84466a28f391a5ff4cf256
LeonardoVileela <leovilela.empresa@gmail.com>
2021-09-10 18:13:10 -0300

    backup

--- src/components/Logo.js
+++
@@ -1,5 +0,0 @@
-const Logo = (props) => (
-  <h2>Ch'aska</h2>
-);
-
-export default Logo;
--- src/constants.js
+++
@@ -1,6 +0,0 @@
-const SERVER = process.env.REACT_APP_SERVER
-
-export const APP_NAME = "BlueTasks"
-export const AUTH_ENDPOINT = `${SERVER}`
-export const API_ENDPOINT = `${SERVER}/api`
-export const JWT_TOKEN_NAME = "Yuumi"
